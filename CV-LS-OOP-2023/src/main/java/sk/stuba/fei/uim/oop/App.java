176df40bdc1adde725cc8b2460faaee2d4f4d4ff
matus kuma <kuma.matus@gmail.com>
2023-02-22 09:45:25 +0100

    Príklad s prednášky č.1 inicializacia a vypis pola

--- CV-LS-OOP-2023/src/main/java/sk/stuba/fei/uim/oop/App.java
+++ CV-LS-OOP-2023/src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -3,7 +3,11 @@ package sk.stuba.fei.uim.oop;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello world"); /*Cvičenie 1 Task 1*/
-        compare(1.2,1.3);
+        int a = compare(1.2,1.3);
+        System.out.println(a);
+        int[] array = new int[]{1,2,3,4,5,6,7,8,9,10};
+        vypisPola(array);
+
     }
 
 
@@ -22,4 +26,9 @@ public class App {
         }
 
     } /*Cvičenie 1 Task 2*/
+    public static void vypisPola(int[] array){
+        for(int i = 0;i<array.length;i++ ){
+            System.out.println("cislo na "+(i+1)+"tej pozici je cislo "+array[i]);
+        }
+    } /*Príklad z prednášky č.1*/
 }
